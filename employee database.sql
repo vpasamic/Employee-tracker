44a02c203de6e01979d38164e69b3d3350341359
ver Pasamic <vpasamic22@gmail.com>
2020-10-13 16:39:43 -0700

    initial commit

---
+++ employee database.sql
@@ -0,0 +1,32 @@
+Drop Database If Exists employee_tracker;
+
+Create Database employee_tracker;
+
+Use employee_tracker;
+
+CREATE TABLE department(
+id integer auto_increment not null,
+name varchar(30) not null,
+primary key(id)
+);
+
+CREATE TABLE role(
+id integer auto_increment not null,
+title varchar(30) not null,
+salary decimal not null,
+department_id Integer not null,
+constraint fk_department_id foreign key (department_id) references department(id),
+primary key(id)
+);
+
+
+CREATE TABLE employee(
+id integer auto_increment not null,
+first_name varchar(30) not null,
+last_name varchar(30) not null,
+role_id integer not null,
+constraint fk_role_id FOREIGN KEY (role_id) REFERENCES role(id),
+manager_id integer ,
+constraint fk_manager_id FOREIGN KEY (manager_id) REFERENCES employee(id),
+Primary key(id)
+);
