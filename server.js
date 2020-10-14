c743c82261eafff81c198ae5e23fae8dd9739199
ver Pasamic <vpasamic22@gmail.com>
2020-10-13 17:40:00 -0700

    add functions working

---
+++ server.js
@@ -0,0 +1,93 @@
+const inquirer=require("inquirer");
+const mysql=require("mysql");
+require("console.table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employee_tracker"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
+
+function start(){
+    inquirer.prompt({
+        name:"start",
+        type:"list",
+        message:"What do you want to do?",
+        choices:["Add","View","Update Employee"]
+    }).then(function (answer){
+        switch (answer.start){
+            case "Add":
+                addoptions()
+                break;
+            case "View":
+                viewoptions()
+                break;
+            case "Update Employee":
+                updateoemployee()
+                break;
+        }
+    }
+    )
+}
+//add functions
+async function addoptions(){
+    inquirer.prompt({
+        name:"types",
+        type:"list",
+        message:"What do you want to add",
+        choices:["Department","Role","Employee"]
+    }).then(function(answer){
+        switch(answer.types){
+            case "Department":
+                addDepartment()
+                break;
+            case "Role":
+                addRole()
+                break;
+            case "Employee":
+                addEmployee()
+                break;
+        }
+            
+    })
+    console.log("addoptions")
+}
+
+
+async function addDepartment(){
+    console.log("adddepartment")
+    start()
+}
+
+async function addRole(){
+    console.log("addRole")
+    start()
+}
+
+async function addEmployee(){
+    console.log("addEmployee")
+    start()
+}
+
+
+
+
+//view functions
+async function viewoptions(){
+    console.log("viewoptions")
+    start()
+}
+
+async function updateoemployee(){
+    console.log("updateemployee")
+    start()
+}
+
+start()
